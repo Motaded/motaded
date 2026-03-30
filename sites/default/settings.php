853ed75ac410fbb4fe8842bb77aa3b53db668b35
Alina Khmilevska <alina.khmilevska.lee@gmail.com>
2026-03-30 17:05:34 +0300

    Fix Status Report errors and warnings

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -909,6 +909,11 @@ $ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
 if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
   require $ddev_settings;
 }
+
+// Private file system (private://) for Webform and other protected uploads.
+// See https://www.drupal.org/psa-2016-003
+$settings['file_private_path'] = $app_root . '/sites/default/files/private';
+
 $settings['simple_sitemap_engines.index_now.key'] = 'c4755d75-1a26-46e7-aad9-7a819eb4f020'; 
 
 $config['system.logging']['error_level'] = 'verbose';
